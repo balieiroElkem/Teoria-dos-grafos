af47e68bea6da1113a85dba5cb7f50bcb990682f
Elkem Balieiro <elkem.balieiroo@gmail.com>
2022-09-03 20:25:18 -0300

    Criando o grafo

--- exercicios/exercicio01/src/main/java/aplicacao/Aplicacao.java
+++ exercicios/exercicio01/src/main/java/aplicacao/Aplicacao.java
@@ -12,14 +12,28 @@ public class Aplicacao {
       grafo.adicionarVertice("B");
       grafo.adicionarVertice("C");
       grafo.adicionarVertice("D");
+      grafo.adicionarVertice("E");
+      grafo.adicionarVertice("F");
+      grafo.adicionarVertice("G");
 
       grafo.conectarVertices("A", "B");
-      grafo.conectarVertices("A", "C");
-      grafo.conectarVertices("A", "D");
+      grafo.conectarVertices("A", "E");
+      grafo.conectarVertices("B", "C");
+      grafo.conectarVertices("B", "D");
+      grafo.conectarVertices("C", "D");
+      grafo.conectarVertices("C", "G");
+      grafo.conectarVertices("D", "E");
+      grafo.conectarVertices("D", "F");
+      grafo.conectarVertices("E", "F");
+      grafo.conectarVertices("F", "G");
 
       System.out.println("Grau do vértice A: " + grafo.getVertice("A").getGrau());
-      System.out.println("Grau do vétice D: " + grafo.getVertice("D").getGrau());
+      System.out.println("Grau do vétice B: " + grafo.getVertice("B").getGrau());
       System.out.println("Grau do vétice C: " + grafo.getVertice("C").getGrau());
+      System.out.println("Grau do vétice D: " + grafo.getVertice("D").getGrau());
+      System.out.println("Grau do vétice E: " + grafo.getVertice("E").getGrau());
+      System.out.println("Grau do vétice F: " + grafo.getVertice("F").getGrau());
+      System.out.println("Grau do vétice G: " + grafo.getVertice("G").getGrau());
 
    }
 }
