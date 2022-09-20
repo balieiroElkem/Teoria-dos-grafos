e1293020b600b3e09f7506224a7c871c5ff26f6c
Elkem Balieiro <84411560+balieiroElkem@users.noreply.github.com>
2022-09-20 19:16:19 -0300

    Add files via upload

---
+++ pratica03/src/main/java/grafo/search/Caminho.java
@@ -0,0 +1,35 @@
+package src.main.java.grafo.search;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Caminho {
+   private Map<String, String> caminho;
+
+   public Caminho() {
+      this.caminho = new HashMap<>();
+   }
+
+   void ligar(String anterior, String proximo) {
+      this.caminho.put(anterior, proximo);
+   }
+
+   public List<String> gerar(String origem, String destino) {
+      List<String> resultado = new ArrayList<>();
+      String no = destino;
+
+      while (no != origem && this.caminho.containsKey(no)) {
+         resultado.add(no);
+         no = this.caminho.get(no);
+      }
+
+      resultado.add(no);
+      Collections.reverse(resultado);
+
+      return resultado;
+   }
+
+}
